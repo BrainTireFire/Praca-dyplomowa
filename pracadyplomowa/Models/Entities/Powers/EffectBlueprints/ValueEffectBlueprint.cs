7c661771ba6c25d65ccba09c89b7ba13f015d619
Maciej Kawęcki <skimiskim12@gmail.com>
2024-06-19 18:00:54 +0200

    Rename DiceSet to uppercase

--- pracadyplomowa/Models/Entities/Powers/EffectBlueprints/ValueEffectBlueprint.cs
+++ pracadyplomowa/Models/Entities/Powers/EffectBlueprints/ValueEffectBlueprint.cs
@@ -8,6 +8,6 @@ namespace pracadyplomowa.Models.Entities.Powers.EffectBlueprints
 {
     public class ValueEffectBlueprint : EffectBlueprint
     {
-        public DiceSet diceSet {get; set;} = null!;
+        public DiceSet DiceSet {get; set;} = null!;
     }
 }

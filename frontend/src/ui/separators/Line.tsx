8cadd44f1d9f50051c241ec7cd40ebcc1b48503f
Maciej Kawęcki <skimiskim12@gmail.com>
2024-06-22 14:06:05 +0200

    Lines and buttons layout

--- frontend/src/ui/separators/Line.tsx
+++ frontend/src/ui/separators/Line.tsx
@@ -1,11 +1,16 @@
 import styled, { css } from "styled-components";
 
 type LineProps = {
-  size?: "verySmall" | "small" | "medium" | "large";
+  size?: "percantage" | "verySmall" | "small" | "medium" | "large";
   align?: "left" | "center" | "right";
+  bold?: "small" | "medium" | "large";
 };
 
 const sizes = {
+  percantage: css`
+    width: 100%;
+    height: 1px;
+  `,
   verySmall: css`
     width: 70vw;
     height: 1px;
@@ -20,6 +25,18 @@ const sizes = {
   `,
   large: css`
     width: 90vw;
+    height: 5px;
+  `,
+};
+
+const bolds = {
+  small: css`
+    height: 1px;
+  `,
+  medium: css`
+    height: 3px;
+  `,
+  large: css`
     height: 5px;
   `,
 };
@@ -41,11 +58,13 @@ const Line = styled.hr<LineProps>`
   background-color: var(--color-header-text);
   ${(props) => sizes[props.size || "large"]}
   ${(props) => aligns[props.align || "center"]}
+  ${(props) => bolds[props.bold || "small"]}
 `;
 
 Line.defaultProps = {
   size: "large",
   align: "center",
+  bold: "small",
 };
 
 export default Line;

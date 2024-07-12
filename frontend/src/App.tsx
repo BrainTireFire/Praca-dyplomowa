7e14e3f8935d0dfdb4fed9efcf50037c77b8b978
Maciej Kawęcki <skimiskim12@gmail.com>
2024-07-12 15:04:54 +0200

    Setting up toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { I18nextProvider } from "react-i18next";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 import "./App.css";
 import GlobalStyles from "./styles/GlobalStyles";
@@ -23,6 +24,27 @@ function App() {
         <I18nextProvider i18n={i18n}>
           <GlobalStyles />
           <Router />
+
+          <Toaster
+            position="top-center"
+            gutter={12}
+            containerStyle={{ margin: "8px" }}
+            toastOptions={{
+              success: {
+                duration: 3000,
+              },
+              error: {
+                duration: 5000,
+              },
+              style: {
+                fontSize: "16px",
+                maxWidth: "500px",
+                padding: "16px 24px",
+                backgroundColor: "var(--color-navbar)",
+                color: "var(--color-secondary-text)",
+              },
+            }}
+          />
         </I18nextProvider>
       </QueryClientProvider>
     </>

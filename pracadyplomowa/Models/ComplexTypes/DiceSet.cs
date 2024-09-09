5014f21f5d994a8c1253a7c1c8242146337955b8
Maciej Romaniuk <75385323+Lokk3n@users.noreply.github.com>
2024-09-09 20:44:16 +0200

    Removed complex types

--- pracadyplomowa/Models/ComplexTypes/DiceSet.cs
+++ pracadyplomowa/Models/ComplexTypes/DiceSet.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace pracadyplomowa.Models.Entities.Characters
 {
-    [ComplexType]
+    [Owned]
     public class DiceSet
     {
         public int d20 { get; set; }

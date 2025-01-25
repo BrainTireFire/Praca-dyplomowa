9fafc43f4652851d21aa813fab690a63b10ef398
Maciej Romaniuk <75385323+Lokk3n@users.noreply.github.com>
2025-01-25 23:18:46 +0100

    Level up restriction

--- pracadyplomowa/Models/Entities/Characters/Character.cs
+++ pracadyplomowa/Models/Entities/Characters/Character.cs
@@ -1584,5 +1584,50 @@ namespace pracadyplomowa.Models.Entities.Characters
                 return Math.Max(baseNumber, maxFromEffects) + additional;
             }
         }
+
+        [NotMapped]
+        public int CurrentLevel{
+            get{
+                return R_CharacterHasLevelsInClass.Count;
+            }
+        }
+
+        [NotMapped]
+        public bool CanLevelUp {
+            get{
+                var experienceRequiredForNextLevel = new Dictionary<int, int>
+                {
+                    { 1, 0 },
+                    { 2, 300 },
+                    { 3, 900 },
+                    { 4, 2700 },
+                    { 5, 6500 },
+                    { 6, 14000 },
+                    { 7, 23000 },
+                    { 8, 34000 },
+                    { 9, 48000 },
+                    { 10, 64000 },
+                    { 11, 85000 },
+                    { 12, 100000 },
+                    { 13, 120000 },
+                    { 14, 140000 },
+                    { 15, 165000 },
+                    { 16, 195000 },
+                    { 17, 225000 },
+                    { 18, 265000 },
+                    { 19, 305000 },
+                    { 20, 355000 }
+                };
+
+                if (CurrentLevel < 20)
+                {
+                    int requiredExperience = experienceRequiredForNextLevel[CurrentLevel + 1];
+
+                    return ExperiencePoints >= requiredExperience;
+                }
+                return false;
+            }
+        }
+
     }
 }

f1dbfc9da52300bf0956b4034185b4be417f565b
Maciej Romaniuk <maciej.romaniuk95@gmail.com>
2025-05-23 20:42:13 +0200

    Increased contrast when disabled

--- frontend/src/ui/forms/Input.tsx
+++ frontend/src/ui/forms/Input.tsx
@@ -35,10 +35,72 @@ const Input = styled.input<InputProps>`
 
   ${(props) => sizes[props.size || "medium"]}
   ${(props) => heights[props.height || "normal"]}
-  /* Custom styles */
-
   ${(props) => props.customStyles}
+
+  &[type="radio"],
+  &[type="checkbox"] {
+    appearance: none;
+    width: 1.1rem;
+    height: 1.1rem;
+    border: 1px solid var(--color-navbar-border);
+    background-color: var(--color-secondary-background);
+    position: relative;
+    padding: 0.7rem;
+    margin: 0;
+  }
+
+  &[type="radio"] {
+    border-radius: 50%;
+
+    &:checked::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 0.9rem;
+      height: 0.9rem;
+      border-radius: 50%;
+      background-color: #555;
+    }
+
+    &:disabled {
+      border-color: #999;
+      background-color: #ccc;
+    }
+
+    &:disabled:checked::after {
+      background-color: #333;
+    }
+  }
+
+  &[type="checkbox"] {
+    border-radius: 3px;
+
+    &:checked::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 0.4rem;
+      height: 0.75rem;
+      border: solid #555;
+      border-width: 0 0.2rem 0.2rem 0;
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+
+    &:disabled {
+      border-color: #999;
+      background-color: #ccc;
+    }
+
+    &:disabled:checked::after {
+      border-color: #333;
+    }
+  }
 `;
+
+
 
 Input.defaultProps = {
   size: "medium",

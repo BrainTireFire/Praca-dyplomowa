b53b245c4c2b34872639015220e8f50e95ab21c4
Maciej Romaniuk <maciej.romaniuk95@gmail.com>
2025-06-08 14:26:28 +0200

    Fixed power updateing

--- pracadyplomowa/Services/Power/IPowerService.cs
+++ pracadyplomowa/Services/Power/IPowerService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using pracadyplomowa.Models.Entities.Powers;
 
 namespace pracadyplomowa.Services.ItemFamily
 {
     public interface IPowerService
     {
-        bool CheckExistenceAndEditAccess(int powerId, int userId, out ActionResult errorResult);
+        public bool CheckExistenceAndEditAccess(int powerId, int userId, out ActionResult errorResult, out Power? power);
+        public bool CheckExistenceAndEditAccess(int powerId, int userId, out ActionResult errorResult);
     }
 }
